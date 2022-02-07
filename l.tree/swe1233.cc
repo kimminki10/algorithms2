c4493f0e4dd44eef75429fbafb61dc21c4e34066
kimminki10 <pangshe10@gmail.com>
2022-02-07 17:32:03 +0900

    swe1233 cc

--- l.tree/swe1233.cc
+++ l.tree/swe1233.cc
@@ -5,34 +5,24 @@
 using namespace std;
 typedef long long ll;
 
-struct Node {
-    char c='\0';
-    int l= -1, r=-1;
-    Node() : Node('\0', -1, -1) {}
-    Node(char ch, int le, int ri): c(ch), l(le), r(ri) {}
-};
-
-Node n[201];
-int N;
-
-void inorder(int idx) {
-    Node c = n[idx];
-    if (c.l != -1) inorder(c.l);
-    if (c.c != '\0') printf("%c", c.c);
-    if (c.r != -1) inorder(c.r);
-}
-
 int solve() {
+    int result = 1;
+    int N;
     scanf("%d", &N);
 
     char line[100];
     for (int i = 0; i < N; i++) {
-        scanf(" %[\n]", line);
+        scanf(" %[^\n]", line);
         int idx, left=-1, right=-1; char op='\0';
         sscanf(line, "%d %c %d %d", &idx, &op, &left, &right);
+
+        if (idx <= N/2) {
+            if ('0' <= op && op <= '9') { result = 0; }
+        } else {
+            if (!('0' <= op && op <= '9')) { result = 0; }
+        }
     }
-
-
+    return result;
 }
 
 void testcase(int tc) {
