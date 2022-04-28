a486bfe528409eed542ae1c7fb569f095e00acfc
kimminki10 <pangshe10@gmail.com>
2022-04-28 17:34:09 +0900

    boj 11780 py

---
+++ i.sortestpath/boj11780.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+m = int(input().rstrip())
+
+jido = [[9876543210] * n for _ in range(n)]
+citys = [[[] for _ in range(n)] for _ in range(n)]
+
+for _ in range(m):
+    a, b, c = list(map(int, input().split()))
+    if jido[a-1][b-1] > c:
+        jido[a-1][b-1] = min(jido[a-1][b-1], c)
+        citys[a-1][b-1] = [a, b]
+
+for i in range(n):
+    jido[i][i] = 0
+    citys[i][i] = 1
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if jido[i][j] > jido[i][k] + jido[k][j]:
+                jido[i][j] = jido[i][k] + jido[k][j]
+                citys[i][j] = citys[i][k] + citys[k][j][1:]
+
+for row in jido:
+    print(' '.join([str(i) for i in row]))
+
+for i in range(n):
+    for j in range(n):
+        if jido[i][j] == 0:
+            print(0)
+        else:
+            print(len(citys[i][j]), ' '.join([str(v) for v in  citys[i][j]]))
