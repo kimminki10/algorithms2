ee11b9898ada52d334b01ed1fde3d511cb3e1ffa
kimminki10 <pangshe10@gmail.com>
2024-12-11 17:59:58 +0900

    boj 1197 kruskal

--- g.greedy/boj1197.py
+++ g.greedy/boj1197.py
@@ -1,33 +1,39 @@
-from heapq import heapify, heappop, heappush
 import sys
 input = sys.stdin.readline
 
+
+class UnionFind:
+    def __init__(self, size: int):
+        self.parent = [ i for i in range(size+1) ]
+        self.rank = [0] * (size+1)
+    
+    def uu(self, a: int, b: int) -> bool:
+        x,y = map(self.ff, [a,b])
+        if x == y: return False
+        if self.rank[x] < self.rank[y]:
+            self.parent[x] = y
+        else:
+            self.parent[y] = x
+            if self.rank[x] == self.rank[y]:
+                self.rank[x]+=1
+        return True
+    
+    def ff(self, x: int) -> int:
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.ff(self.parent[x])
+        return self.parent[x]
+
 V, E = map(int, input().split())
-edges = [[] for _ in range(V)]
+edges = []
 for _ in range(E):
     a,b,c = map(int, input().split())
-    edges[a-1].append([c,b-1])
-    edges[b-1].append([c,a-1])
-
-visited = [False] * V
-def prim(cur):
-    ret = 0
-    to_search = edges[cur]
-    visited[cur] = True
-    heapify(to_search)
-
-    while to_search:
-        c, n = heappop(to_search)
-        if visited[n]: continue
-        visited[n] = True
-        
-        ret += c
-        for item in edges[n]:
-            heappush(to_search, item)
-    return ret
+    edges.append([c, a-1,b-1])
+edges.sort()
 
 ans = 0
-for i in range(V):
-    if visited[i]: continue
-    ans += prim(i)
+uf = UnionFind(V)
+for c,a,b in edges:
+    if uf.uu(a,b):
+        ans += c
 print(ans)
