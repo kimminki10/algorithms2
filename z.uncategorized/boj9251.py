d3ba549e9776c9eefce0c4c88d1b92ec65f3683f
kimminki10 <pangshe10@gmail.com>
2025-03-28 22:40:13 +0900

    boj 9251 python lcs 2

--- z.uncategorized/boj9251.py
+++ z.uncategorized/boj9251.py
@@ -1,18 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-
 A = input().rstrip()
 B = input().rstrip()
-al = len(A)+1
-bl = len(B)+1
-dp = [[0] * al for _ in range(bl)]
 
-for i in range(1, bl):
-    for j in range(1, al):
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        if A[j-1] == B[i-1]:
-            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1)
+dp = [0] * max(len(A), len(B))
+for a in A:
+    m = 0
+    for j, b in enumerate(B):
+        if m < dp[j]:
+            m = dp[j]
+            continue
+        if a == b:
+            dp[j] = m + 1
 
-print(dp[bl-1][al-1])
-        
+print(max(dp))
