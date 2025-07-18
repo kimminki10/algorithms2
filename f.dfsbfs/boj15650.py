53055fb6573ef049a5f3eab2e992b2ff84fe262a
kimminki10 <pangshe10@gmail.com>
2025-07-18 16:46:48 +0900

    refactor: simplify combination logic in boj15650.py using a direct DFS approach

--- f.dfsbfs/boj15650.py
+++ f.dfsbfs/boj15650.py
@@ -2,23 +2,12 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-arr = range(1, N+1)
 
-def combination(l, r):
-    def dfs(n, r, ans):
-        if r == 0:
-            print(' '.join([str(i) for i in ans]))
-            return
-        
-        if ans:
-            small = ans[-1]+1
-        else:
-            small = 1
+def dfs(cur, d, ans):
+    if d == M:
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(cur+1, N+1):
+        dfs(i,d+1, ans+[i])
 
-        for next in range(small, n+1):
-            ans.append(next)
-            dfs(n, r-1, ans)
-            ans.pop()
-    n = len(l)
-    dfs(n, r, [])
-combination(arr, M)
+dfs(0,0, [])
